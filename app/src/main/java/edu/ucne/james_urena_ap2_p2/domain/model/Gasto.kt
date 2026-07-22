c133e85830b76dee8aeadacd30fea003b5d3d210
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2026-07-22 19:58:35 -0400

    Modifica el Gasto.kt

---
+++ app/src/main/java/edu/ucne/james_urena_ap2_p2/domain/model/Gasto.kt
@@ -0,0 +1,10 @@
+package edu.ucne.james_urena_ap2_p2.domain.model
+
+data class Gasto(
+    val gastoId: Int = 0,
+    val fecha: String,
+    val suplidor: String,
+    val ncf: String,
+    val itbis: Double,
+    val monto: Double
+)
