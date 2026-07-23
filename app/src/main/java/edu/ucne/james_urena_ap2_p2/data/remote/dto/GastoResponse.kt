d507bcd281cca7bedc8c88b7eb4060d878a89f5d
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2026-07-22 20:02:15 -0400

    Modifica el GastoResponse.kt

---
+++ app/src/main/java/edu/ucne/james_urena_ap2_p2/data/remote/dto/GastoResponse.kt
@@ -0,0 +1,21 @@
+package edu.ucne.james_urena_ap2_p2.data.remote.dto
+
+import edu.ucne.james_urena_ap2_p2.domain.model.Gasto
+
+data class GastoResponse(
+    val gastoId: Int,
+    val fecha: String,
+    val suplidor: String,
+    val ncf: String,
+    val itbis: Double,
+    val monto: Double
+) {
+    fun toDomain() = Gasto(
+        gastoId = gastoId,
+        fecha = fecha,
+        suplidor = suplidor,
+        ncf = ncf,
+        itbis = itbis,
+        monto = monto
+    )
+}
