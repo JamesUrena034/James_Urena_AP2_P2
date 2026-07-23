c229fa4305b269417bb067128b557a924d3a8150
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2026-07-22 20:02:42 -0400

    Modifica el GetGastosUseCase.kt

---
+++ app/src/main/java/edu/ucne/james_urena_ap2_p2/domain/usecase/GetGastosUseCase.kt
@@ -0,0 +1,10 @@
+package edu.ucne.james_urena_ap2_p2.domain.usecase
+
+import edu.ucne.james_urena_ap2_p2.domain.repository.GastoRepository
+import javax.inject.Inject
+
+class GetGastosUseCase @Inject constructor(
+    private val repository: GastoRepository
+) {
+    operator fun invoke() = repository.getGastos()
+}
