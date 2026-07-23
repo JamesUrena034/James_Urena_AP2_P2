2a4f39d65dd5b3e7d86162c2a8caed5276e82f07
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2026-07-22 20:04:08 -0400

    Modifica el Screen.kt

--- app/src/main/java/edu/ucne/james_urena_ap2_p2/presentation/navigation/Screen.kt
+++ app/src/main/java/edu/ucne/james_urena_ap2_p2/presentation/navigation/Screen.kt
@@ -1,4 +1,15 @@
 package edu.ucne.james_urena_ap2_p2.presentation.navigation
 
-class Screen {
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
+
+sealed class Screen : NavKey {
+    @Serializable
+    data object GastoList : Screen()
+
+    @Serializable
+    data object GastoCreate : Screen()
+
+    @Serializable
+    data class GastoEdit(val id: Int) : Screen()
 }
