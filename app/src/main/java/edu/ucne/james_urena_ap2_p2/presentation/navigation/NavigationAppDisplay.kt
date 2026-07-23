b52e43b48d715285ed925c7a845dfcb15026bae2
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2026-07-22 20:03:13 -0400

    Modifica el NavigationAppDisplay.kt

--- app/src/main/java/edu/ucne/james_urena_ap2_p2/presentation/navigation/NavigationAppDisplay.kt
+++ app/src/main/java/edu/ucne/james_urena_ap2_p2/presentation/navigation/NavigationAppDisplay.kt
@@ -1,4 +1,42 @@
 package edu.ucne.james_urena_ap2_p2.presentation.navigation
 
-class NavigationAppDisplay {
+import androidx.compose.runtime.Composable
+import androidx.navigation3.runtime.entryProvider
+import androidx.navigation3.runtime.rememberNavBackStack
+import androidx.navigation3.ui.NavDisplay
+import edu.ucne.james_urena_ap2_p2.presentation.form.GastoFormScreen
+import edu.ucne.james_urena_ap2_p2.presentation.list.GastoListScreen
+
+@Composable
+fun AppNavigationDisplay() {
+    val backStack = rememberNavBackStack(Screen.GastoList)
+
+    NavDisplay(
+        backStack = backStack,
+        onBack = { backStack.removeLastOrNull() },
+        entryProvider = entryProvider {
+            entry<Screen.GastoList> {
+                GastoListScreen(
+                    onGastoClick = { gastoId ->
+                        backStack.add(Screen.GastoEdit(gastoId))
+                    },
+                    onAddClick = {
+                        backStack.add(Screen.GastoCreate)
+                    }
+                )
+            }
+            entry<Screen.GastoCreate> {
+                GastoFormScreen(
+                    onSaved = { backStack.removeLastOrNull() },
+                    onBack = { backStack.removeLastOrNull() }
+                )
+            }
+            entry<Screen.GastoEdit> { key ->
+                GastoFormScreen(
+                    onSaved = { backStack.removeLastOrNull() },
+                    onBack = { backStack.removeLastOrNull() }
+                )
+            }
+        }
+    )
 }
